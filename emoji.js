30501bff02a48f8eef0efb708d445674bbb536a7
riahk <moar.riah@gmail.com>
2015-08-15 19:43:58 -0700

    add delete confirmation, styling

--- emoji.js
+++ emoji.js
@@ -19,29 +19,43 @@ if(localStorage.hasOwnProperty('emojis')) {
 $(document).ready(function() {
   //list all emoji in the bank
   //var emojis = JSON.parse(localStorage['emojis']);
-  for (var face in emojis) {
-    var emoji = emojis[face];
-    $('.bank').append("<li class=\"emoji\"><input value=\""+emoji+"\" readonly></input><p class=\"copy hidden\">ctrl/cmd + c to copy!</p><button class=\"delete hidden\">delete?</button></li>");
-  }
+  var update = function() {
+    $('.bank').empty();
+    for (var face in emojis) {
+      var emoji = emojis[face];
+      $('.bank').append("<li class=\"emoji\"><input value=\""+emoji+"\" readonly></input><p class=\"copy hidden\">ctrl/cmd + c to copy!</p><button class=\"delete hidden\">delete?</button><div class=\"confirm hidden\"><p>are you sure?</p><span><button class=\"yes\">yes</button><button class=\"no\">no</button></span></div></li>");
+    }
+  };
+
+  update();
 
   //select emoji when clicked
   $('.bank').on('click', '.emoji', function(event) {
-    $($(".selected").children()[1]).addClass("hidden");
-    $($(".selected").children()[2]).addClass("hidden");
-    $(".selected").removeClass("selected");
-    $(this).addClass("selected");
-    $($(this).children()[1]).removeClass("hidden");
-    $($(this).children()[2]).removeClass("hidden");
-    $(this).children()[0].select();
+    if(event.target.className !== 'delete') {
+      $($(".selected").children()[1]).addClass("hidden");
+      $($(".selected").children()[2]).addClass("hidden");
+      $($(".selected").children()[3]).addClass("hidden");
+      $(".selected").removeClass("selected");
+      $(this).addClass("selected");
+      $($(this).children()[1]).removeClass("hidden");
+      $($(this).children()[2]).removeClass("hidden");
+      $(this).children()[0].select();
+    }
   });
 
   $('.bank').on('click', '.delete', function(event) {
-    console.log('delete functionality here');
-    deleteEmoji = $($(this).parent().children()[0]).val();
-    console.log(deleteEmoji);
+    var confirmed = $($(this).parent().children()[3]);
+    confirmed.removeClass("hidden");
+  });
+
+  $('.bank').on('click', '.yes', function(event) {
+    //console.log($(this).parent().parent().parent().children()[0]);
+    deleteEmoji = $($(this).parent().parent().parent().children()[0]).val();
+    //console.log(deleteEmoji);
     delete emojis[deleteEmoji];
-    console.log(emojis);
+    //console.log(emojis);
     localStorage['emojis'] = JSON.stringify(emojis);
+    update();
   });
 
   //display form to add emoji
@@ -63,6 +77,7 @@ $(document).ready(function() {
       localStorage['emojis'] = JSON.stringify(emojis);
       $('#newEmoji').val('');
       $('#addForm').addClass("hidden");
+      update();
     }
   });
 
