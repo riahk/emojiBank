25942324983ed6caef621cec9082ce9f41ab4186
riahk <moar.riah@gmail.com>
2015-08-25 19:33:17 -0700

    form to add emoji

--- emojiBankReact/js/emoji.js
+++ emojiBankReact/js/emoji.js
@@ -1,3 +1,11 @@
+var emojiObj = JSON.parse(window.localStorage.getItem('emojis'));
+console.log(emojiObj);
+
+var emojis =  [];
+for(var key in emojiObj) {
+  emojis.push(emojiObj[key]);
+}
+
 var Title = React.createClass({
   render: function() {
     return (
@@ -18,31 +26,91 @@ var EmojiList = React.createClass({
   },
 
   render: function() {
+    var emojis = this.props.data.map(function(emoji) {
+      return (
+        <Emoji emoji={emoji} />
+      );
+    });
+
     return (
       <ul>
-        <li><Emoji emoji="(///▽///)" /></li>
+      {emojis}
       </ul>
     );
   }
 });
 
 var Emoji = React.createClass({
+  select: function(event) {
+    $($(event.target).children()[0]).select();
+  },
+
+
   render: function() {
     return (
-      <input value={this.props.emoji} readonly></input>
+      <li onClick={this.select}>
+        <input value={this.props.emoji} readOnly></input>
+      </li>
     );
   }
 });
+
+var AddInput = React.createClass({
+  render: function() {
+    return (
+      <input></input>
+    );
+  }
+});
+
+var AddButton = React.createClass({
+  addEmoji: function(event) {
+    console.log('add emoji functionality here');
+    var inputfield = $($(event.target).parent().children()[0]);
+    var emoji = inputfield.val();
+    
+    //add emoji to localStorage
+    if(emoji.length > 0) {
+      emojiObj[emoji] = emoji;
+      window.localStorage.setItem('emojis', JSON.stringify(emojiObj));
+      inputfield.val('');
+    }
+
+  },
+
+  render: function() {
+    return (
+      <button onClick={this.addEmoji}>Add</button>
+    );
+  }
+});
+
+var AddForm = React.createClass({
+  render: function() {
+    return (
+      <div>
+        <AddInput />
+        <AddButton />
+      </div>
+    );
+  }
+});
+
+
+/*window.localStorage.setItem('emojis', JSON.stringify({ "(///▽///)": "(///▽///)", 
+      "(((o(*ﾟ▽ﾟ*)o)))": "(((o(*ﾟ▽ﾟ*)o)))" }));*/
 
 var Content = React.createClass({
   render: function() {
     return (
       <div>
         <Title />
-        <EmojiList />
+        <EmojiList data={this.props.data}/>
+        <AddForm />
       </div>
     );
   }
 });
 
-React.render(<Content />, document.getElementById('content'));
+React.render(<Content data={emojis} />, document.getElementById('content'));
+
