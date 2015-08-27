e3a79d642750fc4419d054c119333e8124da9870
riahk <moar.riah@gmail.com>
2015-08-27 14:08:57 -0700

    add styling

--- emojiBankReact/js/emoji.js
+++ emojiBankReact/js/emoji.js
@@ -10,16 +10,16 @@ var taggedEmojis = {
   }
 };
 
-window.localStorage.setItem('emojis', JSON.stringify(taggedEmojis));
+/*window.localStorage.setItem('emojis', JSON.stringify(taggedEmojis));*/
 
-var emojiObj = JSON.parse(window.localStorage.getItem('emojis'));
-console.log(emojiObj);
+//var emojiObj = JSON.parse(window.localStorage.getItem('emojis'));
+//console.log(emojiObj);
 
 var Title = React.createClass({
   render: function() {
     return (
       <div>
-        <h1>EMOJIS!!</h1>
+        <h3 className="title">emojiBank</h3>
       </div>
     );
   }
@@ -33,7 +33,7 @@ var EmojiList = React.createClass({
   render: function() {
     var emojis = this.props.data.map(function(emoji) {
       return (
-        <Emoji emoji={emoji} />
+        <Emoji key={emoji.id} emoji={emoji} />
       );
     });
 
@@ -47,17 +47,27 @@ var EmojiList = React.createClass({
 
 var Emoji = React.createClass({
   select: function(event) {
+    $('.msg').addClass('hidden');
+    $('.selected').removeClass('selected');
     if(event.target.tagName === 'INPUT') {
       $(event.target).select();
+      var div = $(event.target).parent();
+      var msg = $(div).children()[1];
+      $(msg).removeClass('hidden');
+      $(div).addClass('selected');
     } else {
         $($(event.target).children()[0]).select();
+        var msg = $(event.target).children()[1];
+        $(msg).removeClass('hidden');
+        $(event.target).addClass('selected');
       }
   },
 
   render: function() {
     return (
-      <li onClick={this.select}>
-        <input onClick={this.select} value={this.props.emoji} readOnly></input>
+      <li key={this.props.key} onClick={this.select}>
+        <input key={this.props.key} onClick={this.select} value={this.props.emoji} readOnly></input>
+        <p className="msg hidden">cmd/ctrl + c!</p>
       </li>
     );
   }
@@ -66,7 +76,11 @@ var Emoji = React.createClass({
 var AddInput = React.createClass({
   render: function() {
     return (
-      <input></input>
+      <div>
+        <input placeholder="add an emoji"></input>
+        <br />
+        <input placeholder="tags (sep. by comma)"></input>
+      </div>
     );
   }
 });
@@ -74,17 +88,39 @@ var AddInput = React.createClass({
 var AddButton = React.createClass({
   addEmoji: function(event) {
     console.log('add emoji functionality here');
-    var inputfield = $($(event.target).parent().children()[0]);
-    var emoji = inputfield.val();
+    var div = $($(event.target).parent().children()[0]);
+    console.log(div);
+
+    var emojifield = $(div).children()[0];
+    var tagfield = $(div).children()[2];
+
+    console.log(emojifield);
+    console.log(tagfield);
+
+    var emoji = $(emojifield).val();
+    var tags = $(tagfield).val().split(',');
+
+    console.log(tags);
+    console.log(emoji);
     
     //add emoji to localStorage
     if(emoji.length > 0) {
-      emojiObj[emoji] = emoji;
-      window.localStorage.setItem('emojis', JSON.stringify(emojiObj));
-      inputfield.val('');
+      var newemojiObj = JSON.parse(window.localStorage.getItem('emojis'));
+
+      var newEmoji = {};
+      newEmoji['emoji'] = emoji;
+      newEmoji['tags'] = tags;
+      newemojiObj[emoji] = newEmoji;
+
+      console.log(newEmoji);
+
+      window.localStorage.setItem('emojis', JSON.stringify(newemojiObj));
+
+      $(emojifield).val('');
+      $(tagfield).val('');
     }
+
     this.props.changeEmojis();
-    //need to alert that emoji has been added, somehow...
   },
 
   render: function() {
@@ -154,22 +190,24 @@ var Content = React.createClass({
 
   changeEmojis: function() {
     console.log('successfully bubbled up!');
+    console.log(JSON.parse(window.localStorage.getItem('emojis')));
     this.setState({ emojiObj: JSON.parse(window.localStorage.getItem('emojis')) });
   },
 
   render: function() {
     var emojis =  [];
-    for(var key in emojiObj) {
+    console.log(this.state.emojiObj);
+    for(var key in this.state.emojiObj) {
       if(this.state.filter.length > 0) { //if there's a filter, use it
         //cycle through the emoji's tags to see if they match
-        var tags = emojiObj[key].tags;
+        var tags = this.state.emojiObj[key].tags;
         for(var i = 0; i < tags.length; i++) {
           if(tags[i] === this.state.filter) {
-            emojis.push(emojiObj[key].emoji);
+            emojis.push(this.state.emojiObj[key]['emoji']);
             break;
           }
         }
-      } else { emojis.push(emojiObj[key].emoji); }
+      } else { emojis.push(this.state.emojiObj[key]['emoji']); }
     }
 
     return (
